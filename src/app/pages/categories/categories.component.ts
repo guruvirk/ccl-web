0bfd3b108640e5e0ef6fd0a93313c79b2da9624d
Gurpinder <gurpinder.singh@mindfulsas.com>
2020-08-23 19:42:09 +0530

    fixes

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -3,7 +3,8 @@ import { RoleService } from '../../services/role.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UxService } from '../../services/ux.service';
 import { SubCatgoryService } from 'src/app/services/sub-catgory.service';
-import { SubCategory } from 'src/app/models';
+import { SubCategory, Category } from 'src/app/models';
+import { CatgoryService } from 'src/app/services/catgory.service';
 
 @Component({
   selector: 'app-categories',
@@ -13,9 +14,11 @@ import { SubCategory } from 'src/app/models';
 export class CategoriesComponent implements OnInit, OnDestroy {
 
   categories: SubCategory[];
+  category: Category
   isLoading = false;
 
   constructor(private api: SubCatgoryService,
+    private categoryApi: CatgoryService,
     private route: ActivatedRoute,
     private auth: RoleService,
     private router: Router,
@@ -23,9 +26,12 @@ export class CategoriesComponent implements OnInit, OnDestroy {
     this.route.params.subscribe(params => {
       if (params.id) {
         this.isLoading = true;
-        this.api.search({ category: params.id }).subscribe(page => {
-          this.categories = page.items
-          this.isLoading = false;
+        this.categoryApi.get(params.id).subscribe(item => {
+          this.category = item
+          this.api.search({ category: item.id }).subscribe(page => {
+            this.categories = page.items
+            this.isLoading = false;
+          })
         })
       }
     })
